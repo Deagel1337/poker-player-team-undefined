8d3cff2260772fa80d1c89b688045c036b01474c
Nicolai niggl Ort <info@nicolai-ort.com>
2023-04-14 11:04:45 +0000

    use this.bet

--- src/Player.ts
+++ src/Player.ts
@@ -17,19 +17,21 @@ export class Player {
       this.bet(stack, "quads", betCallback)
     }
     if (this.isFullHouse(allCards) === true) {
-      console.log("Fullhouse")
       let raise = minRaise + stack/2
-      if( raise > stack) betCallback(stack)
+      if( raise > stack){
+        this.bet(stack, "fullhouse", betCallback)
+      }
       else 
-        betCallback(raise)
+        this.bet(raise, "fullhouse-raise", betCallback)
     }
     if (this.isFlush(allCards) === true) {
       console.log("Flush")
       let raise = minRaise + stack/2
-      if( raise > stack) betCallback(stack)
+      if( raise > stack){
+        this.bet(stack, "flush", betCallback)
+      }
       else 
-        betCallback(raise)
-      // this.bet(minRaise + raiseUnit*allCards.length, "fullhouse", betCallback)
+        this.bet(raise, "flush-raise", betCallback)
     }
     if (this.isFlush(allCards) === true) {
       this.bet(minRaise + raiseUnit*allCards.length, "flush", betCallback)
@@ -39,8 +41,7 @@ export class Player {
     }
     if (this.isTrips(allCards) === true) {
       console.log("trips")
-      betCallback(minRaise + 2*raiseUnit*allCards.length)
-      // this.bet(minRaise + raiseUnit*allCards.length, "trips", betCallback)
+      this.bet(minRaise + 2*raiseUnit*allCards.length, "trips", betCallback)
     }
     if (this.isTwoPair(allCards) === true) {
       betCallback(minRaise + raiseUnit*allCards.length)
