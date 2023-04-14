12aac0ccf5e979dae43a6762567faf6761cdd918
Nicolai niggl Ort <info@nicolai-ort.com>
2023-04-14 08:53:55 +0000

    findindex -> filter

--- src/Player.ts
+++ src/Player.ts
@@ -1,26 +1,23 @@
 export class Player {
-  isPair(cards:Array<GameCard>):boolean{
+  isPair(cards: Array<GameCard>): boolean {
     for (const card of cards) {
-      const i = cards.findIndex((c) => {
-        c.rank == card.rank
-      })
-      if(i !== -1) return true
+      if (cards.filter((c) => c.rank == card.rank).length > 0) return true
     }
     return false
   }
   public betRequest(gameState: GameState, betCallback: (bet: number) => void): void {
     let cardValues
-    if(this.isPair(gameState.community_cards.concat(...gameState.players[gameState.in_action].hole_cards)) === true){
+    if (this.isPair(gameState.community_cards.concat(...gameState.players[gameState.in_action].hole_cards)) === true) {
       betCallback(gameState.current_buy_in - gameState.players[gameState.in_action].bet + gameState.minimum_raise + 5)
     }
-        for (let card of gameState.players[gameState.in_action].hole_cards) {
-            cardValues += this.toRank(card.rank)
-        }
-        if(cardValues > 20)
-          betCallback(gameState.current_buy_in - gameState.players[gameState.in_action].bet + gameState.minimum_raise);
-        
-        betCallback(gameState.current_buy_in - gameState.players[gameState.in_action].bet)
+    for (let card of gameState.players[gameState.in_action].hole_cards) {
+      cardValues += this.toRank(card.rank)
     }
+    if (cardValues > 20)
+      betCallback(gameState.current_buy_in - gameState.players[gameState.in_action].bet + gameState.minimum_raise);
+
+    betCallback(gameState.current_buy_in - gameState.players[gameState.in_action].bet)
+  }
 
   public showdown(gameState: {
     players: Array<GamePlayer>
@@ -29,12 +26,12 @@ export class Player {
   }
 
   toRank(cardRank: string): number {
-    if(cardRank == "J") return 10;
-    if(cardRank == "Q") return 11;
-    if(cardRank == "K") return 12;
-    if(cardRank == "A") return 13;
+    if (cardRank == "J") return 10;
+    if (cardRank == "Q") return 11;
+    if (cardRank == "K") return 12;
+    if (cardRank == "A") return 13;
     return parseInt(cardRank)
-}
+  }
 };
 
 
@@ -56,7 +53,7 @@ export type GameState = {
   community_cards: Array<GameCard>
 }
 
-export type GameCard= {
+export type GameCard = {
   rank: string,
   suit: string;
 }
